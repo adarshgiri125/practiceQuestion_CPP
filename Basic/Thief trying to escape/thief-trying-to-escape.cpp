85d1ed770857388a12a594d053b1bef680642816
Adarsh Giri <67739727+adarshgiri125@users.noreply.github.com>
2023-06-28 13:10:43 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Thief trying to escape/thief-trying-to-escape.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    int totalJumps(int X, int Y, int N, int arr[]){
+        int count = 0;
+        
+        int i = 0;
+        while(i < N){
+            
+            if(arr[i] <= X){
+                i++;
+                count++;
+            }
+            else{
+                arr[i] = arr[i] - (X-Y);
+                count++;
+            }
+            
+        }
+        return count++;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int X, Y, N;
+        cin>>X>>Y>>N;
+        int arr[N];
+        for(int i = 0;i < N;i++)
+            cin>>arr[i];
+        
+        Solution ob;
+        cout<<ob.totalJumps(X, Y, N, arr)<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
