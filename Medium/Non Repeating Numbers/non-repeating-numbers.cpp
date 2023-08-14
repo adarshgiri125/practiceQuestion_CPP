10d5216a6ae8d94c4d636b566f157393a946e22c
Adarsh Giri <67739727+adarshgiri125@users.noreply.github.com>
2023-08-14 18:46:42 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Non Repeating Numbers/non-repeating-numbers.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    vector<int> singleNumber(vector<int> nums) 
+    {
+        vector<int>v;
+        sort(nums.begin(),nums.end());
+        int l=0;
+        while(l<nums.size()){
+            if(nums[l]==nums[l+1]) l=l+2;
+            else {
+                v.push_back(nums[l]);
+                l++;
+             }
+        }
+        sort(v.begin(),v.end());
+        return v;
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+    int T;
+    cin >> T;
+    while(T--)
+    {
+    	int n; 
+    	cin >> n;
+    	vector<int> v(2 * n + 2);
+    	for(int i = 0; i < 2 * n + 2; i++)
+    		cin >> v[i];
+    	Solution ob;
+    	vector<int > ans = ob.singleNumber(v);
+    	for(auto i: ans)
+    		cout << i << " ";
+    	cout << "\n";
+    }
+	return 0;
+}
+// } Driver Code Ends
