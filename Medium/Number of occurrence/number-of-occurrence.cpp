da790d34230c291331a95748bddaafe19c882284
Adarsh Giri <67739727+adarshgiri125@users.noreply.github.com>
2023-08-20 10:20:33 +0530

    Updated Solution - GfG to GitHub

--- Medium/Number of occurrence/number-of-occurrence.cpp
+++ Medium/Number of occurrence/number-of-occurrence.cpp
@@ -10,36 +10,37 @@ class Solution{
 public:	
 	/* if x is present in arr[] then returns the count
 		of occurrences of x, otherwise returns 0. */
-int count(int arr[], int n, int x) {
-        int s = 0;
-        int e = n - 1;
-        int pos = -1; 
-        
-            while (s <= e) { 
-                int mid = s + (e - s) / 2; 
-                
-                if (x < arr[mid]) {
-                    e = mid - 1;
-                } else if (x > arr[mid]) {
-                    s = mid + 1;
-                } else {
-                    pos = mid; 
-                    e = mid - 1;
-                }
-            }
-    
-            if (pos == -1) {
-                return 0; 
-            }
-    
-            int count = 0;
-            for (int i = pos; i < n && arr[i] == x; i++) { 
-                count++;
-            }
-        
-        return count;
-    }
-
+	int count(int arr[], int n, int x) {
+	     int s = 0;
+	     int e = n-1;
+	  
+	     
+	     while(s <= e){
+	         int mid = (s + e) / 2;
+	         if(arr[mid] == x){
+	             e = mid - 1;
+	         }
+	         else if(arr[mid] > x){
+	             e = mid - 1;
+	         }
+	         else{
+	             s = mid + 1;
+	         }
+	         
+	        
+	     }
+	     
+	    
+	  
+	     int count = 0;
+	     for(int i = s; i<n; i++){
+	         if(arr[i] == x){
+	             count++;
+	         }
+	     }
+	     
+	     return count;
+	}
 };
 
 //{ Driver Code Starts.
