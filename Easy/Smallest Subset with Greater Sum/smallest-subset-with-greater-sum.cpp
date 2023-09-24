598b1929aaa4844df68b57308820ff258aa8fd43
Adarsh Giri <67739727+adarshgiri125@users.noreply.github.com>
2023-09-24 11:24:46 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Smallest Subset with Greater Sum/smallest-subset-with-greater-sum.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    int minSubset(vector<int> &arr,int n){
+        
+        sort(arr.begin(), arr.end());
+        long long sum = 0;
+        for(int i = 0; i<n; i++) sum += arr[i];
+        
+        long long check = 0;
+        int value = 0;
+        for(int i = n-1; i>=0; i--){
+            sum -= arr[i];
+            check += arr[i];
+            if( check > sum){
+                value++;
+                return value;
+            }
+            else{
+                value++;
+            }
+        }
+        return value;
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        vector<int> Arr(N);
+        for(int i=0;i<N;i++){
+            cin>>Arr[i];
+        }
+        Solution ob;
+        cout<<ob.minSubset(Arr,N)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
