2f10024f1eb28bd218abeea983f7ebb7cd40764b
Adarsh Giri <67739727+adarshgiri125@users.noreply.github.com>
2023-10-04 08:06:04 +0530

    Updated Solution - GfG to GitHub

--- Easy/Roman Number to Integer/roman-number-to-integer.cpp
+++ Easy/Roman Number to Integer/roman-number-to-integer.cpp
@@ -11,27 +11,28 @@ using namespace std;
 class Solution {
   public:
     int romanToDecimal(string &str) {
-        map<char,int> mp;
+        unordered_map<char,int> mp;
+        mp['I'] = 1;
+        mp['V'] = 5;
+        mp['X'] = 10;
+        mp['L'] = 50;
+        mp['C'] = 100;
+        mp['D'] = 500;
+        mp['M'] = 1000;
         
-        mp.insert({'I',1});
-        mp.insert({'V', 5});
-        mp.insert({'X', 10});
-        mp.insert({'L', 50});
-        mp.insert({'C',100});
-        mp.insert({'D', 500});
-        mp.insert({'M',1000});
-        int sum = 0;
+        int ans = 0;
         for(int i = 0; i<str.length(); i++){
-            if(mp[str[i]] < mp[str[i+1]]){
-                sum += mp[str[i+1]] - mp[str[i]];
-                i++;
+            if(i == str.length() - 1){
+                ans += mp[str[i]];
             }
-            else{
-                sum += mp[str[i]];
+            else if(mp[str[i]] < mp[str[i+1]]){
+                ans -= mp[str[i]];
+            }
+            else if(mp[str[i]] >= mp[str[i+1]]){
+                ans += mp[str[i]];
             }
         }
-        
-        return sum;
+        return ans;
     }
 };
 
