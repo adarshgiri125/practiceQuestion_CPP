467e5b993415d8ea0c73a42169bd13358301a88e
Adarsh Giri <67739727+adarshgiri125@users.noreply.github.com>
2023-10-22 19:47:39 +0530

    Updated Solution - GfG to GitHub

--- Medium/Number of Coins/number-of-coins.cpp
+++ Medium/Number of Coins/number-of-coins.cpp
@@ -11,7 +11,7 @@ class Solution{
     int solve(int coins[], int m, int v, vector<vector<int> > &dp) {
         // Base cases
         if (v == 0) return 0;
-        if (v < 0 || m < 0) return INT_MAX - 1;
+        if (v < 0 || m < 0) return INT_MAX-1;
         if (dp[m][v] != -1) return dp[m][v];
     
         
@@ -23,12 +23,28 @@ class Solution{
         return dp[m][v] = ans;
     }
     
-    int minCoins(int coins[], int m, int v) {
-        vector<vector<int> > dp(m, vector<int>(v + 1, -1));
-        sort(coins, coins + m);
-        int result = solve(coins, m - 1, v, dp);
+    int minCoins(int coins[], int n, int val) {
+        vector<vector<int> > dp(n, vector<int>(val + 1, -1));
+        // sort(coins, coins + m);
+        // int result = solve(coins, m - 1, v, dp);
     
-        return (result >= INT_MAX - 1) ? -1 : result;
+        // return (result >= INT_MAX - 1) ? -1 : result;
+        for(int i = 0; i<n; i++) dp[i][0] = 0;
+        for(int m = 1; m < n; m++){
+            for(int v = 0; v<= val; v++){
+                
+                int useCoin = 1e9;
+                if( v - coins[m] >= 0) useCoin = 1 + solve(coins, m, v - coins[m], dp);
+                int skipCoin = 1e9;
+                if(m - 1 >= 0) skipCoin = solve(coins, m - 1, v, dp);
+                int ans = min(useCoin, skipCoin);
+                
+                dp[m][v] = ans;
+                
+            }
+        }
+        if(dp[n-1][val] >= 1e9) return -1;
+        return dp[n-1][val];
     }
 
 	  
