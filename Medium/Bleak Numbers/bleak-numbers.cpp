c92d656466c6586c9e05b79fcbce25f8e6f786ee
Adarsh Giri <67739727+adarshgiri125@users.noreply.github.com>
2023-10-28 09:30:58 +0530

    Updated Solution - GfG to GitHub

--- Medium/Bleak Numbers/bleak-numbers.cpp
+++ Medium/Bleak Numbers/bleak-numbers.cpp
@@ -6,20 +6,19 @@ using namespace std;
 class Solution
 {
 public:
+    int count(int n){
+        int count = 0;
+        while(n > 0){
+            if(n&1) count++;
+            n = n >> 1;
+        }
+        return count;
+    }
 	int is_bleak(int n)
 	{
-	    if(n<=100){
-	        for(int i=1;i<=100;i++){
-	            int x=__builtin_popcount(i);
-	            if(x+i==n) return 0;
-	        }
-	        return 1;
-	    }
 	    
-	    for(int i=32;i>=1;i--){
-	        int x=__builtin_popcount(n-i);
-	        
-	        if(n-i+x==n) return 0;
+	    for(int i = n-32;i<=n;i++){
+	        if(i + count(i) == n) return 0;
 	    }
 	    return 1;
 	}
