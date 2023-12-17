cb712284da14cc1f7d4313c8d35fcbd4c9eb2ba0
Adarsh Giri <67739727+adarshgiri125@users.noreply.github.com>
2023-12-17 16:31:11 +0530

    Updated Solution - GfG to GitHub

--- Easy/Max Sum without Adjacents/max-sum-without-adjacents.cpp
+++ Easy/Max Sum without Adjacents/max-sum-without-adjacents.cpp
@@ -8,46 +8,21 @@ using namespace std;
 //User function template for C++
 class Solution{
 public:	
-/// -------->>>>>>>> Memoization Recursion <<<<<----------
-//     int solve(int *arr, int n,vector<int> &dp){
-//         if(n == 0) return arr[n];
-//         if(n < 0) return 0;
-//         if(dp[n] != -1) return dp[n];
+    int solve(int *arr, int ind,vector<int> &dp){
+        if(ind < 0){
+            return 0;
+        }
+        if(dp[ind] != -1) return dp[ind];
+        int value = arr[ind] + solve(arr, ind - 2,dp);
+        int value2 = solve(arr, ind - 1,dp);
         
-//         int first = arr[n] + solve(arr,n-2,dp);
-//         int second = 0 + solve(arr,n-1,dp);
-        
-//         return dp[n] = max(first,second);
-//     }
-// 	// calculate the maximum sum with out adjacent
-// 	int findMaxSum(int *arr, int n) {
-// 	    // code here
-// 	    vector<int> dp(n,-1);
-// 	    return solve(arr,n-1,dp);
-// 	}
-
-   /// ----->>>>>> optimize approach with space optimize <<<-------
-   
-   int findMaxSum(int *arr, int n) {
-
-	   int prev = arr[0];
-	   int prev2 = 0;
-	   
-	   for(int i = 1; i<n; i++){
-	       int first = arr[i];
-	       if(i > 1) first += prev2;
-	       
-	       int second = 0 + prev;
-	       
-	       int value = max(first,second);
-	       
-	       prev2 = prev;
-	       prev = value;
-	   }
-	   
-	   return prev;
+        return dp[ind] = max(value , value2);
+    }
+	// calculate the maximum sum with out adjacent
+	int findMaxSum(int *arr, int n) {
+	   vector<int> dp(n+1, -1);
+	   return solve(arr,n-1,dp);
 	}
-   
 };
 
 //{ Driver Code Starts.
