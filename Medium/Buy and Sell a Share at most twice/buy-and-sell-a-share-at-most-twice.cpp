2caec9f4e6f5ee7c4346673089709c84c94148fe
Adarsh Giri <67739727+adarshgiri125@users.noreply.github.com>
2024-02-23 19:05:20 +0530

    Updated Solution - GfG to GitHub

--- Medium/Buy and Sell a Share at most twice/buy-and-sell-a-share-at-most-twice.cpp
+++ Medium/Buy and Sell a Share at most twice/buy-and-sell-a-share-at-most-twice.cpp
@@ -5,46 +5,35 @@ using namespace std;
 
 
 // } Driver Code Ends
-//User function Template for C++
 
-int solve(vector<int>&prices, int n, int buy,int cap, int ind,vector<vector<vector<int> > > &dp){
-        if(cap == 0 || ind == n) return 0;
-        int profit = 0;
-        if(dp[ind][buy][cap] != -1) return dp[ind][buy][cap];
-        if(buy){
-            profit = max(-prices[ind] + solve(prices, n,0,cap, ind+1,dp), 
-                               0 + solve(prices,n,1,cap,ind+1,dp)); 
-        }
-        else{
-            profit = max(prices[ind] + solve(prices,n,1,cap - 1,ind+1,dp),
-                                0 + solve(prices,n,0,cap,ind+1,dp));
-        }
-        return dp[ind][buy][cap] = profit;
-}
-
-int maxProfit(vector<int>&prices){
-   int n = prices.size();
-   vector<vector<vector<int> > > dp(n+1, vector<vector<int> > (2,vector< int > (3,0)));
-   
-   
-   for(int ind = n-1; ind>= 0; ind--){
-       for(int buy = 0; buy <= 1; buy++){
-           for(int cap = 1; cap <= 2; cap++){
+class Solution
+{
+    public:
+        //User function Template for C++
+        
+       int maxProfit(vector<int> &price){
+            int n = price.size();
+            vector<int> leftProfit(n), rightProfit(n);
+            
+            int minPrice = price[0];
+            for(int i = 1;i < n; i++) {
+                minPrice = min(minPrice, price[i]);
+                leftProfit[i] = max(leftProfit[i-1], price[i]-minPrice);
+            }
+            
+            int maxPrice = price[n-1];
+            for(int i = n-2;i >= 0; i--) {
+                maxPrice = max(maxPrice, price[i]);
+                rightProfit[i] = max(rightProfit[i+1], maxPrice-price[i]);
+            }
+            
+            int maxTotalProfit = 0;
+            for(int i = 0;i < n; i++)
+                maxTotalProfit = max(maxTotalProfit, leftProfit[i]+rightProfit[i]);
                 
-                if(buy == 1){
-                    dp[ind][buy][cap] = max(-prices[ind] + dp[ind+1][0][cap], 
-                                       0 + dp[ind+1][1][cap]); 
-                }
-                else{
-                   dp[ind][buy][cap] = max(prices[ind] +dp[ind+1][1][cap-1],
-                                        0 + dp[ind+1][0][cap]);
-                }
-             
-           }
-       }
-   }
-   return dp[0][1][2];
-}
+            return maxTotalProfit;
+        }
+};
 
 //{ Driver Code Starts.
 
@@ -57,7 +46,8 @@ int main(){
         cin>>n;
         vector<int> price(n);
         for(int i=0;i<n;i++) cin>>price[i];
-        cout<<maxProfit(price)<<endl;
+        Solution obj;
+        cout<<obj.maxProfit(price)<<endl;
     }
 
 }
