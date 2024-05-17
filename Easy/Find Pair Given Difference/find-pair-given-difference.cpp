2e2654e839b04890df5e2a2fc8f3fe7fcbf01c73
Adarsh Giri <67739727+adarshgiri125@users.noreply.github.com>
2024-05-17 12:40:11 +0530

    Updated Solution - GfG to GitHub

--- Easy/Find Pair Given Difference/find-pair-given-difference.cpp
+++ Easy/Find Pair Given Difference/find-pair-given-difference.cpp
@@ -1,46 +1,73 @@
 //{ Driver Code Starts
-#include<bits/stdc++.h>
- 
-using namespace std; 
+#include <bits/stdc++.h>
+using namespace std;
 
-
-bool findPair(int arr[], int size, int n);
-
-int main()
-{
-    int t;
-    cin>>t;
-    while(t--)
-    {
-        int l,n;
-        cin>>l>>n;
-        int arr[l];
-        for(int i=0;i<l;i++)
-            cin>>arr[i];
-        if(findPair(arr, l, n))
-            cout<<1<<endl;
-        else cout<<"-1"<<endl;
-    }
-    
-  
-    return 0;
-}
-// } Driver Code Ends
-
-
-bool findPair(int arr[], int size, int n){
-    sort(arr,arr+size);
-    int i=0,j=i+1;
-    while(i<size && j<size){
-        if(i==j)j++;
-        if(arr[j]-arr[i] == n){
-            return true;
-        }else if(arr[j] - arr[i] < n){
-            j++;
-        }else {
-            i++;
+class Array {
+  public:
+    template <class T>
+    static void input(vector<T> &A, int n) {
+        for (int i = 0; i < n; i++) {
+            scanf("%d ", &A[i]);
         }
     }
-    return false;
-    
+
+    template <class T>
+    static void print(vector<T> &A) {
+        for (int i = 0; i < A.size(); i++) {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int findPair(int n, int x, vector<int> &arr) {
+        // code here
+        unordered_map<int,int>m;
+        for(auto it:arr){
+            m[it]++;
+            if(x==0 && m[it]==2){
+                return 1;
+            }
+        }
+        if(x==0){
+            return -1;
+        }
+        for(auto it:arr){
+            if(m.find(x+it)!=m.end()){
+                return 1;
+            }
+        }
+        return -1;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    scanf("%d ", &t);
+    while (t--) {
+
+        int n;
+        scanf("%d", &n);
+
+        int x;
+        scanf("%d", &x);
+
+        vector<int> arr(n);
+        Array::input(arr, n);
+
+        Solution obj;
+        int res = obj.findPair(n, x, arr);
+
+        cout << res << endl;
+    }
 }
+
+// } Driver Code Ends
