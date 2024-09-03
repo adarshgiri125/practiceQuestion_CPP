d3107eb934d6f5ff72606b468049d6a1de13da6d
Adarsh Giri <67739727+adarshgiri125@users.noreply.github.com>
2024-09-03 10:02:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum number of deletions and insertions./minimum-number-of-deletions-and-insertions..cpp
+++ Difficulty: Medium/Minimum number of deletions and insertions./minimum-number-of-deletions-and-insertions..cpp
@@ -9,21 +9,20 @@ class Solution{
 	public:
 	int solve(string str1, string str2, int i, int j, vector<vector<int>> &dp) {
     // Base cases
-    if (i >= str1.size()) return str2.size() - j; // If str1 is fully traversed, we need to insert the remaining characters of str2
-    if (j >= str2.size()) return str1.size() - i; // If str2 is fully traversed, we need to delete the remaining characters of str1
+    if (i >= str1.size()) return str2.size() - j;
+    if (j >= str2.size()) return str1.size() - i; 
     
     if (dp[i][j] != -1) return dp[i][j];
     
     if (str1[i] == str2[j]) {
-        // If characters match, no operation needed, move to the next characters in both strings
+       
         return dp[i][j] = solve(str1, str2, i + 1, j + 1, dp);
     } else {
-        // Calculate the costs of removing, inserting, and replacing
-        int remove = 1 + solve(str1, str2, i + 1, j, dp); // Remove current character from str1
-        int insert = 1 + solve(str1, str2, i, j + 1, dp); // Insert current character of str2 into str1
-        int replace = 2 + solve(str1, str2, i + 1, j + 1, dp); // Replace current character of str1 with str2's character
-        
-        // Take the minimum of the three operations
+       
+        int remove = 1 + solve(str1, str2, i + 1, j, dp); 
+        int insert = 1 + solve(str1, str2, i, j + 1, dp); 
+        int replace = 2 + solve(str1, str2, i + 1, j + 1, dp);
+      
         return dp[i][j] = min({remove, insert, replace});
     }
 }
@@ -31,9 +30,35 @@ class Solution{
 int minOperations(string str1, string str2) {
     int n = str1.size();
     int m = str2.size();
-    vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1)); // Adjust dimensions to n+1 x m+1
-    return solve(str1, str2, 0, 0, dp);
+    vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0)); 
+
+    
+    for (int j = 0; j <= m; j++) {
+        dp[n][j] = m - j;
+    }
+    
+    for (int i = 0; i <= n; i++) {
+        dp[i][m] = n - i;
+    }
+
+   
+    for (int i = n - 1; i >= 0; i--) {
+        for (int j = m - 1; j >= 0; j--) {
+            if (str1[i] == str2[j]) {
+                dp[i][j] = dp[i + 1][j + 1]; 
+            } else {
+                int remove = 1 + dp[i + 1][j];
+                int insert = 1 + dp[i][j + 1]; 
+                int replace = 2 + dp[i + 1][j + 1]; 
+                dp[i][j] = min({remove, insert, replace});
+            }
+        }
+    }
+
+   
+    return dp[0][0];
 }
+
 
 };
 
