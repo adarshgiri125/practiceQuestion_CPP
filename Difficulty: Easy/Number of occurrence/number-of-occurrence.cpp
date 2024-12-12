f0b8222b60418111a957be8cdf55eae7bed32430
Adarsh Giri <67739727+adarshgiri125@users.noreply.github.com>
2024-12-12 14:50:42 +0530

    Updated Solution - GfG to GitHub

---
+++ Difficulty: Easy/Number of occurrence/number-of-occurrence.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+// Initial function template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int countFreq(vector<int>& arr, int target) {
+        // Find the first index where target appears using lower_bound
+        int lb = lower_bound(arr.begin(), arr.end(), target) - arr.begin();
+        
+        // Find the first index after the last occurrence of target using upper_bound
+        int ub = upper_bound(arr.begin(), arr.end(), target) - arr.begin();
+        
+        // The difference between upper and lower bounds gives the count of target
+        return ub - lb;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int test_case;
+    cin >> test_case;
+    cin.ignore();
+    while (test_case--) {
+
+        int d;
+        vector<int> arr, brr, crr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        getline(cin, input);
+        ss.clear();
+        ss.str(input);
+        while (ss >> number) {
+            crr.push_back(number);
+        }
+        d = crr[0];
+        int n = arr.size();
+        Solution ob;
+        int ans = ob.countFreq(arr, d);
+        cout << ans << endl;
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
