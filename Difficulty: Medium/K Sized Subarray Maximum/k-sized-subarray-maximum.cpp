d126278974f2378b3c17d00db57942eb84c70d7b
Adarsh Giri <67739727+adarshgiri125@users.noreply.github.com>
2026-01-12 17:56:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K Sized Subarray Maximum/k-sized-subarray-maximum.cpp
+++ Difficulty: Medium/K Sized Subarray Maximum/k-sized-subarray-maximum.cpp
@@ -1,67 +1,26 @@
-//{ Driver Code Starts
-// Initial Template for C++
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-// User function template for C++
-
 class Solution {
   public:
-  //queue
-    // Function to find maximum of each subarray of size k.
-    vector<int> max_of_subarrays(int k, vector<int> &arr) {
+    vector<int> maxOfSubarrays(vector<int>& arr, int k) {
+        // code here
         deque<int> dq;
-        int n = arr.size();
-        int maxi = -1;
-        vector<int> ans;
         int j = 0;
-        for(int i = 0; i<n; i++){
-            while(!dq.empty() && arr[i] >= arr[dq.back()]){
+        vector<int> ans;
+        
+        for(int i = 0; i<arr.size(); i++){
+            while(!dq.empty() && arr[dq.back()] <=  arr[i]){
                 dq.pop_back();
             }
             dq.push_back(i);
-            if(i - j + 1 == k){
+            if(j > dq.front()){
+                dq.pop_front();
+                    
+            }
+            
+            if(i - j + 1 >= k){
                 ans.push_back(arr[dq.front()]);
-                if(dq.front() == j) dq.pop_front();
                 j++;
             }
-          
         }
         return ans;
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-
-    int t;
-    cin >> t;
-    cin.ignore();
-
-    while (t--) {
-
-        string ks;
-        getline(cin, ks);
-        int k = stoi(ks);
-        vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        Solution obj;
-        vector<int> res = obj.max_of_subarrays(k, arr);
-        for (int i = 0; i < res.size(); i++)
-            cout << res[i] << " ";
-        cout << endl;
-    }
-
-    return 0;
-}
-
-// } Driver Code Ends
